bb86c1f93a9ae133195fac504b6a3d58278845ea
Gaston <gastoncorrea90e@gmail.com>
2022-05-11 16:23:00 -0300

    Se hizo un ruteo con pagina de inicio login y pagina de perfil curriculum

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {AppRoutingModule} from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
@@ -10,9 +11,9 @@ import { AptitudesComponent } from './componentes/aptitudes/aptitudes.component'
 import { LogrosComponent } from './componentes/logros/logros.component';
 import { PortfolioService } from './servicios/portfolio.service';
 import {HttpClientModule } from '@angular/common/http';
-import {AppRoutingModule} from './app-routing/app-routing.module';
 import { CurriculumComponent } from './componentes/curriculum/curriculum.component';
 import { LoginComponent } from './componentes/login/login.component';
+
 @NgModule({
   declarations: [
     AppComponent,
