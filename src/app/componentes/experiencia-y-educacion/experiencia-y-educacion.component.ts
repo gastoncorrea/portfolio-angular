ffc60da0878dcaa603b71bcca56f590955955116
Gaston <gastoncorrea90e@gmail.com>
2022-05-18 15:08:23 -0300

    Se hizo form en el modal de Experiencia y se comenzo con las validaciones

--- src/app/componentes/experiencia-y-educacion/experiencia-y-educacion.component.ts
+++ src/app/componentes/experiencia-y-educacion/experiencia-y-educacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-experiencia-y-educacion',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiencia-y-educacion.component.css']
 })
 export class ExperienciaYEducacionComponent implements OnInit {
+  form: FormGroup;
+  valorNombre:any;
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder) {
+    this.form = formBuilder.group({
+      nombre:['',[Validators.maxLength(20),Validators.required]],
+      puesto:['',[Validators.maxLength(20),Validators.required]],
+      logo:['',[Validators.maxLength(50)]],
+      fecha_inicio:['',[Validators.required]],
+      fecha_fin:['',[Validators.required]],
+      tiempo_trab:['',[Validators.required]]
+    })
+   }
 
   ngOnInit(): void {
   }
 
+  get Nombre(){
+    this.valorNombre = this.form.get('nombre');
+    return this.valorNombre;
+  }
 }
