59152603962bf3fd6c4532f65f672c06bf7186c0
Gaston <gastoncorrea90e@gmail.com>
2022-05-18 17:58:42 -0300

    Se hizo validaciones del formulario del modal de experiencia

--- src/app/componentes/experiencia-y-educacion/experiencia-y-educacion.component.ts
+++ src/app/componentes/experiencia-y-educacion/experiencia-y-educacion.component.ts
@@ -9,15 +9,20 @@ import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class ExperienciaYEducacionComponent implements OnInit {
   form: FormGroup;
   valorNombre:any;
+  valorPuesto:any;
+  valorLogo:any;
+  valorInicio:any;
+  valorFin: any;
+  valorTotal:any;
 
   constructor(private formBuilder:FormBuilder) {
     this.form = formBuilder.group({
-      nombre:['',[Validators.maxLength(20),Validators.required]],
+      nombre:['',[Validators.maxLength(50),Validators.required]],
       puesto:['',[Validators.maxLength(20),Validators.required]],
       logo:['',[Validators.maxLength(50)]],
       fecha_inicio:['',[Validators.required]],
       fecha_fin:['',[Validators.required]],
-      tiempo_trab:['',[Validators.required]]
+      tiempo_trab:['',[Validators.required, Validators.maxLength(50)]]
     })
    }
 
@@ -27,5 +32,31 @@ export class ExperienciaYEducacionComponent implements OnInit {
   get Nombre(){
     this.valorNombre = this.form.get('nombre');
     return this.valorNombre;
+  }
+
+  get Puesto(){
+    this.valorPuesto = this.form.get('puesto');
+    return this.valorPuesto;
+  }
+
+  get Logo(){
+    this.valorLogo = this.form.get('logo');
+    return this.valorLogo;
+  }
+
+  get Inicio(){
+    this.valorInicio = this.form.get('fecha_inicio');
+    console.log(this.valorInicio.status);
+    return this.valorInicio;
+  }
+
+  get Fin(){
+    this.valorFin = this.form.get('fecha_fin');
+    return this.valorFin;
+  }
+
+  get Total(){
+    this.valorTotal = this.form.get('tiempo_trab');
+    return this.valorTotal;
   }
 }
