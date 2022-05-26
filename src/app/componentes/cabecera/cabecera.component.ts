8495fa5889b2a8bc640ac7c4a5d69ee968a0df55
Gaston <gastoncorrea90e@gmail.com>
2022-05-26 12:41:27 -0300

    Se creo form para editar persona y residencia desde una ventana modal

--- src/app/componentes/cabecera/cabecera.component.ts
+++ src/app/componentes/cabecera/cabecera.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
 import { PortfolioService } from '../../servicios/portfolio.service';
 
 @Component({
@@ -8,8 +11,93 @@ import { PortfolioService } from '../../servicios/portfolio.service';
 })
 export class CabeceraComponent implements OnInit {
   cabecera: any;
+  // formulario para persona
+  form: FormGroup;
+  // formulario para residencia
+  formResidencia: FormGroup;
 
-  constructor(private portfolioService: PortfolioService) {}
+  // valor que se obtienen del get de los formularios
+  valorNombre:any;
+  valorApellido:any;
+  valorPerfil:any;
+  valorLinkedin:any;
+  valorCelular:any;
+
+  valorDireccion:any;
+  valorLocalidad:any;
+  valorProvincia:any;
+  valorPais:any;
+  valorNacionalidad:any;
+
+
+  constructor(private portfolioService: PortfolioService,
+              private formBuilder: FormBuilder) {
+
+                this.form = this.formBuilder.group({
+                  nombre: ['',[Validators.required, Validators.maxLength(50)]],
+                  apellido: ['',[Validators.required, Validators.maxLength(50)]],
+                  imagen_perfil: ['',[Validators.required, Validators.maxLength(100)]],
+                  url_linkedin: ['',[Validators.required, Validators.maxLength(100)]],
+                  num_celular: ['',[Validators.required, Validators.maxLength(20)]]
+                })
+
+                this.formResidencia = this.formBuilder.group({
+                  direccion: ['',[Validators.required, Validators.maxLength(50)]],
+                  localidad: ['',[Validators.required, Validators.maxLength(50)]],
+                  provincia: ['',[Validators.required, Validators.maxLength(50)]],
+                  pais: ['',[Validators.required, Validators.maxLength(50)]],
+                  nacionalidad: ['',[Validators.required, Validators.maxLength(50)]]
+                })
+              }
+  
+  // metodos get para traer valores de form Persona
+  get Nombre(){
+    this.valorNombre = this.form.get('nombre');
+    return this.valorNombre;
+  }
+  get Apellido(){
+    this.valorApellido = this.form.get('apellido');
+    return this.valorApellido;
+  }
+  get Perfil(){
+    this.valorPerfil = this.form.get('imagen_perfil');
+    return this.valorPerfil;
+  }
+  get Url(){
+    this.valorLinkedin = this.form.get('url_linkedin');
+    return this.valorLinkedin;
+  }
+  get Celular(){
+    this.valorCelular = this.form.get('num_celular');
+    return this.valorCelular;
+  }
+
+  // metodo para traer valores de form residencia
+
+  get Direccion(){
+    this.valorDireccion = this.form.get('direccion');
+    return this.valorDireccion;
+  }
+
+  get Localidad(){
+    this.valorLocalidad = this.form.get('localidad');
+    return this.valorLocalidad;
+  }
+
+  get Provincia(){
+    this.valorProvincia = this.form.get('provincia');
+    return this.valorProvincia;
+  }
+
+  get Pais(){
+    this.valorPais = this.form.get('pais');
+    return this.valorPais;
+  }
+
+  get Nacionalidad(){
+    this.valorNacionalidad = this.form.get('nacionalidad');
+    return this.valorNacionalidad;
+  }
 
   ngOnInit(): void {
     // this.portfolioService.obtenerDatos().subscribe(data => {
