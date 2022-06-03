f1611e642197d08c2453ee25db8f053e8f631b9d
Gaston <gastoncorrea90e@gmail.com>
2022-06-02 23:16:23 -0300

    Se programo para que aparezca en el form el elemento a editar

--- src/app/componentes/aptitudes/aptitudes.component.ts
+++ src/app/componentes/aptitudes/aptitudes.component.ts
@@ -1,69 +1,81 @@
 import { Component, OnInit } from '@angular/core';
 
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {AptitudService} from '../../servicios/aptitud.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AptitudService } from '../../servicios/aptitud.service';
 
 @Component({
   selector: 'app-aptitudes',
   templateUrl: './aptitudes.component.html',
-  styleUrls: ['./aptitudes.component.css']
+  styleUrls: ['./aptitudes.component.css'],
 })
 export class AptitudesComponent implements OnInit {
-  cabecera: any;
+  persona: any;
+  aptitudes: any;
   // form que contienen los input para agregar y editar Skills
   form: FormGroup;
-  formEditar:FormGroup;
+  formEditar: FormGroup;
   // Editarariables que contienen los estados de los inputs para agregar y editar skills
-  valorNombre:any;
-  valorNivel:any;
-  valorNombreEditar:any;
-  valorNivelEditar:any;
+  valorNombre: any;
+  valorNivel: any;
+  valorNombreEditar: any;
+  valorNivelEditar: any;
 
-  constructor(private formBuilder: FormBuilder,
-              private aptitudService: AptitudService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private aptitudService: AptitudService
+  ) {
     this.form = formBuilder.group({
-      nombre:['',[Validators.required, Validators.maxLength(50)]],
-      nivel:['',[Validators.required]]
-    })
+      nombre: ['', [Validators.required, Validators.maxLength(50)]],
+      nivel: ['', [Validators.required]],
+    });
     this.formEditar = formBuilder.group({
-      nombreEditar:['',[Validators.required, Validators.maxLength(50)]],
-      nivelEditar:['',[Validators.required]]
-    })
-   }
-
-   enviar(e:Event){
-     e.preventDefault();
-
-     if(this.form.valid){
-       console.log('form skill valido');
-     }else{
-     }
-   }
-
-  ngOnInit(): void {
-    this.aptitudService.obtenerDatos().subscribe(data => {
-      console.log("Educacion data" + data);
-      this.cabecera = data.aptitud;
+      nombreEditar: ['', [Validators.required, Validators.maxLength(50)]],
+      nivelEditar: ['', [Validators.required]],
     });
   }
-
-  get Nombre(){
+  get Nombre() {
     this.valorNombre = this.form.get('nombre');
     return this.valorNombre;
   }
 
-  get Nivel(){
+  get Nivel() {
     this.valorNivel = this.form.get('nivel');
     return this.valorNivel;
   }
-  get NombreEditar(){
+  get NombreEditar() {
     this.valorNombreEditar = this.form.get('nombreEditar');
     return this.valorNombreEditar;
   }
 
-  get NivelEditar(){
+  get NivelEditar() {
     this.valorNivelEditar = this.form.get('nivelEditar');
     return this.valorNivelEditar;
   }
 
+  ngOnInit(): void {
+    this.aptitudService.obtenerDatos().subscribe((data) => {
+      console.log('Educacion data' + data);
+      this.persona = data;
+      this.aptitudes = data.aptitud;
+    });
+  }
+
+  enviar(e: Event) {
+    e.preventDefault();
+
+    if (this.form.valid) {
+      console.log('form skill valido');
+    } else {
+    }
+  }
+
+  cargarDatosModal(skill:any){
+
+    this.formEditar.setValue({
+      nombreEditar:skill.nombre,
+      nivelEditar:skill.nivel
+    })
+    console.log("Valor de formEditar APTITUDES:")
+    console.log(this.formEditar.value)
+  }
 }
